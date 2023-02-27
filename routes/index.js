ba794bcf104ea1e9ff5488366aeaf052389e028f
Feermeringer <Tu correo>
2023-02-27 14:27:58 -0300

    configuracion front y bakc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+import userRouter from './users.js'
 import express from 'express'
+
 let router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
 })
+router.use("/users", userRouter )
 
 export default router
